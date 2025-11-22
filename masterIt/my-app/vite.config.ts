851efe9f396f418d99146526487d82cc85e0a7f5
JaiVaibhav <jaivaibhav16@gmail.com>
2025-11-22 06:47:17 +0000

    Add preview configuration to Vite for local development

--- masterIt/my-app/vite.config.ts
+++ masterIt/my-app/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   preview: {
+    host: "0.0.0.0",
+    port: 4173,                     // local default; Render will override via $PORT if you pass it
+    allowedHosts: ["componentstomasterreact.onrender.com"],
+  },
 })
